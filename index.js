a23103dd96969a9e756cd873bb73d0a84190ed27
Aagam Shah <78636155+aagamshah2211@users.noreply.github.com>
2021-08-20 16:27:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,45 @@ for (let i = 0; i < aob.length; i++) {
     }
 }
 console.log(newArray);
+
+
+// Task 1
+
+let arr = []
+
+for (let i = 0; i < 1000; i++) {
+    var obj = {
+        name: makeid(),
+        age: randomNumber(1, 100),
+        XP: randomNumber(0, 800),
+        Level: randomNumber(1, 9),
+    }
+
+    var score = obj.XP / obj.Level
+
+    arr.push({...obj, score })
+}
+
+console.log("ARR", arr);
+
+arr.sort((a, b) => {
+    return a.score < b.score ? 1 : b.score < a.score ? -1 : 0
+})
+
+console.log("Sorting Array", arr);
+
+
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
